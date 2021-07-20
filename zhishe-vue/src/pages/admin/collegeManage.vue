1c8d9e494d4486c8db5651475e01b5d55cd7b713
ACupofAir <timethoughts@outlook.com>
2021-07-21 01:22:43 +0800

    format

--- zhishe-vue/src/pages/admin/collegeManage.vue
+++ zhishe-vue/src/pages/admin/collegeManage.vue
@@ -31,7 +31,7 @@
                 <i class="el-icon-chat-dot-round" style="font-weight: bold;">评论数量 {{curCollegeCommentNum}}</i>
               </el-row>
             </el-row>
-            </span>
+          </span>
         </span>
 
       </div>
