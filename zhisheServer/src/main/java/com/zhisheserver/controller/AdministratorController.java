59fd00b84dc6acb34a81de11372a9d10d2d950ed
Yicheng. Bao <60834356+thisisbadBao@users.noreply.github.com>
2021-07-23 18:39:30 +0800

    update 7.23

--- zhisheServer/src/main/java/com/zhisheserver/controller/AdministratorController.java
+++ zhisheServer/src/main/java/com/zhisheserver/controller/AdministratorController.java
@@ -1,17 +1,10 @@
 package com.zhisheserver.controller;
 
-
-<<<<<<< HEAD
-import com.zhisheserver.service.AdministratorService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-=======
 import com.zhisheserver.dto.Login;
 import com.zhisheserver.service.AdministratorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
->>>>>>> 048a851755f83d62347b87a885c653863ddb9bf4
+
 
 import org.springframework.stereotype.Controller;
 
