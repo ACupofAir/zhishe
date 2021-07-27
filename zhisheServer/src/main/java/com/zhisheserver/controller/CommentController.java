0dbc8dcf31148e4d82482107a18c748e74eaa310
Yicheng. Bao <60834356+thisisbadBao@users.noreply.github.com>
2021-07-27 15:41:18 +0800

    update 7.27

--- zhisheServer/src/main/java/com/zhisheserver/controller/CommentController.java
+++ zhisheServer/src/main/java/com/zhisheserver/controller/CommentController.java
@@ -3,15 +3,11 @@ package com.zhisheserver.controller;
 
 import com.zhisheserver.dto.Login;
 import com.zhisheserver.entity.Comment;
-<<<<<<< HEAD
 import com.zhisheserver.entity.Info;
 import com.zhisheserver.mapper.CommentMapper;
 import com.zhisheserver.service.CommentService;
 import com.zhisheserver.service.InfoService;
 import org.apache.ibatis.annotations.Select;
-=======
-import com.zhisheserver.service.CommentService;
->>>>>>> 7b199dcf1d86baf3e1a8b1d241db1ed7dea5c090
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -47,7 +43,6 @@ public class CommentController {
         return this.commentService.getCommentByName(campus);
     }
 
-<<<<<<< HEAD
     @PostMapping("/post")
     @ResponseBody
     public Object saveComment(
@@ -59,8 +54,5 @@ public class CommentController {
     }
 
 
-
-=======
->>>>>>> 7b199dcf1d86baf3e1a8b1d241db1ed7dea5c090
 }
 
